17968244fe96187838813a62d81ce5bf6577ff42
Patrick Rećko <guest_none@protonmail.com>
2018-12-20 15:05:23 +0100

    Add Copyrights for current files
    - remove unnecessary file

---
+++ Source/SShootLib/SShootField.h
@@ -0,0 +1,9 @@
+//---------------------------------------------------------------------------
+/*  SPDX-License-Identifier: Expat
+	
+	Copyright (C) 2018 Patrick Rećko, Yan Yanutsevich, Paweł Krzywosz
+	
+	This source file is released under the MIT License.
+	See LICENSE.md for full terms. This notice is not to be removed.
+*/
+//---------------------------------------------------------------------------
