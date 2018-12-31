c9ed97c1ed74b0503fc246339becc6323f6022f0
woodensalmon <45913039+woodensalmon@users.noreply.github.com>
2018-12-31 18:59:35 +0100

    Update SShootGameplay.c
    nie wiedzialem gdzie to umiescic wiec umieszczam tutaj, w razie czego prosze to przeniesc
    jak się jednak sposób przechowywania danych sie nie podoba to pisac
    rozdzielilem funkcje aktualizującą pozycje obiektów na 3 funkcje, ponieważ różne obiekty będą się poruszać z różną prędkością
     zapraszam do kodzenia bo w tym tempie nie zrobimy projektu XD

--- Source/SShootLib/SShootGameplay.c
+++ Source/SShootLib/SShootGameplay.c
@@ -8,3 +8,52 @@
 */
 //---------------------------------------------------------------------------
 
+
+char obstacles[2][16];
+char bullets[2][16];
+char powerUpXPos;
+char powerUpYPos;
+char powerUpType;
+char playerYPos=0;
+
+void gmplUpdateObjectsPositions()
+{
+	//
+	obstacles[0][0]=0;
+	obstacles[1][0]=0;
+	
+	for(int i=0;i<2;i++)
+	{
+		for(int u=1;u<16;u++)
+		{
+			if(obstacles[i][u]!=0)
+			{
+				obstacles[i][u-1]=obstacles[i][u];
+				
+			}
+		}
+	}
+}
+void gmplUpdateBulltetsPositions()
+{
+	bulltes[0][0]=0;
+	bullets[1][0]=0;
+	for(int i=0;i<2;i++)
+	{
+		for(int u=1;u<16;u++)
+		{
+			if(bullets[u]!=0)
+			{
+				bullets[i][u-1]=bullets[i][u];
+				
+			}
+		}
+	}
+}
+void gmplUpdatePowerUpPosition()
+{
+	if(powerUpXpos!=0)
+	{
+		powerUpXpos--;
+	}
+}
