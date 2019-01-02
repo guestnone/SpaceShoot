0fc8a2ac7ecf384526cc4db7956ee67379f22298
woodensalmon <45913039+woodensalmon@users.noreply.github.com>
2019-01-02 11:31:28 +0100

    Update SShootGameplay.c
    przerobiłem kod. Teraz zamiast przepisywać tablicę, przesuwam jej początek, a aktualizuję tylko jej koniec

--- Source/SShootLib/SShootGameplay.c
+++ Source/SShootLib/SShootGameplay.c
@@ -16,6 +16,10 @@ char powerUpYPos;
 char powerUpType;
 char playerYPos=0;
 
+char beginOfObstacles = 0;
+char beginOfBullets   = 0;
+char beginOfPowerUp   = 0;
+
 typedef enum powerUpType
 {
 	HPUp,
@@ -27,10 +31,15 @@ typedef enum powerUpType
 
 void gmplUpdateObjectsPositions()
 {
+	beginOfObstacles=(beginOfObstacles+1)%16;
+	obstacles[0][beginOfObstacles]=0;
+	obstacles[1][beginOfObstacles]=0;
 	
+	
+	
+	/*
 	obstacles[0][0]=0;
-	obstacles[1][0]=0;
-	
+	obstacles[0][0]=0;
 	for(int i=0;i<2;i++)
 	{
 		for(int u=1;u<16;u++)
@@ -41,10 +50,15 @@ void gmplUpdateObjectsPositions()
 				obstacles[i][u]=0;
 			}
 		}
-	}
+	}*/
 }
 void gmplUpdateBulltetsPositions()
 {
+	beginOfBullets=(beginOfBullets+1)%16;
+	bullets[0][beginOfBullets=0;
+	bullets[1][beginOfBullets]=0;
+	
+	/*
 	bulltes[0][0]=0;
 	bullets[1][0]=0;
 	for(int i=0;i<2;i++)
@@ -59,18 +73,65 @@ void gmplUpdateBulltetsPositions()
 			}
 		}
 	}
+	*/
 }
 void gmplUpdatePowerUpPosition()
 {
+	
+	beginOfPowerUp=(beginOfPowerUp+1)%16;
+	
+	/*
 	if(powerUpXpos!=0)
 	{
 		powerUpXpos--;
 	}
+	*/
 }
 
 void gmplResolveObjectCollision()
 {	
 	// do napisania funkcje HpDown
+	
+	if(playerYPos == 0 && obstacles[0][beginOfObstacles]!=0)
+	{
+		HpDown();	
+	}
+	if(playerYPos == 1&& obstacles[1][beginOfObstacles]!=0)
+	{
+		HpDown();
+	}
+	
+	if(playerYPos == 0 && bullets[0][beginOfBullets]!=0)
+	{
+		HpDown();	
+	}
+	if(playerYPos == 1&& bullets[1][BeginOfBullets]!=0)
+	{
+		HpDown();
+	}
+	
+	if(playerYPos == powerUpYPos && beginOfPowerUp == powerUpXPos)
+	{
+		//do napisania: funkcje do odpalania PowerUpów
+		switch(powUpType)
+		{
+			case HPUp:
+				
+				break;
+			case Defense:
+				
+				break;
+			case LaserPowerUp:
+				
+				break;
+			case DecreaseSpeed:
+				
+				break;
+		}
+	}
+	
+	
+	/*
 	if(playerYPos == 0 && obstacles[0][0]!=0)
 	{
 		HpDown();	
@@ -107,7 +168,7 @@ void gmplResolveObjectCollision()
 				
 				break;
 		}
-	}
+	}*/
 	
 }
 
