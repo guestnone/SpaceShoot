92437fcb2f62ca3cba13f76a1f83e3add813e154
woodensalmon <45913039+woodensalmon@users.noreply.github.com>
2019-01-06 17:22:42 +0100

    Update SShootGameplay.c
    przenosze kod z brancha pobocznego, do brancha dev/Paweł

--- Source/SShootLib/SShootGameplay.c
+++ Source/SShootLib/SShootGameplay.c
@@ -15,10 +15,20 @@ char powerUpXPos;
 char powerUpYPos;
 char powerUpType;
 char playerYPos=0;
-
+char playerXpos=0;
 char beginOfObstacles = 0;
 char beginOfBullets   = 0;
 char beginOfPowerUp   = 0;
+char lifesLeft=3;
+
+char idOfBullet=0;
+typedef struct playerBullet
+{
+	char x;
+	char y;
+	char isShot=0;
+} playerBullets[2];
+
 
 typedef enum powerUpType
 {
@@ -28,64 +38,59 @@ typedef enum powerUpType
 	DecreaseSpeed
 }powUpType;
 
-
+void gmplUpdatePlayerXPos()
+{
+	playerXPos=(playerXPos+1)%16;	
+}
 void gmplUpdateObjectsPositions()
 {
 	beginOfObstacles=(beginOfObstacles+1)%16;
 	obstacles[0][beginOfObstacles]=0;
 	obstacles[1][beginOfObstacles]=0;
-	
-	
-	
-	/*
-	obstacles[0][0]=0;
-	obstacles[0][0]=0;
-	for(int i=0;i<2;i++)
-	{
-		for(int u=1;u<16;u++)
+}
+
+void gmplDetectPlayerBulletCollision()
+{
+		for(char i=0;i<2;++i)
 		{
-			if(obstacles[i][u]!=0)
+			char y = playerBullets[i].y;
+			char x=(playerBullets[i].x + beginOfObstacles )%16;
+			if(obstacles[y][x]==2)
 			{
-				obstacles[i][u-1]=obstacles[i][u];
-				obstacles[i][u]=0;
+				enemyIsDown(y,x);	
 			}
 		}
-	}*/
 }
-void gmplUpdateBulltetsPositions()
+void gmplPlayerShootBullet()
+{
+	playerBullets[idOfBullet].y=playerYPos;
+	playerBullets[idOfBullet].x=0;
+}
+void gmplMovePlayerBullets()
+{
+	for(char i=0;i<2;++i)
+	{
+		if(playerBullets[i].isShot==true)
+		{
+			playerBullets[i].x=(playerBullets[i].x+1)%16;	
+		}
+	}
+}
+void gmplEnemyShootBullet(char y)
+{
+	bullets[y][beginOfBullets]=1;
+}
+void gmplUpdateEnemyBulltetsPositions()
 {
 	beginOfBullets=(beginOfBullets+1)%16;
 	bullets[0][beginOfBullets=0;
 	bullets[1][beginOfBullets]=0;
-	
-	/*
-	bulltes[0][0]=0;
-	bullets[1][0]=0;
-	for(int i=0;i<2;i++)
-	{
-		for(int u=1;u<16;u++)
-		{
-			if(bullets[u]!=0)
-			{
-				bullets[i][u-1]=bullets[i][u];
-				bullters[i][u]=0;
-				
-			}
-		}
-	}
-	*/
 }
 void gmplUpdatePowerUpPosition()
 {
 	
 	beginOfPowerUp=(beginOfPowerUp+1)%16;
 	
-	/*
-	if(powerUpXpos!=0)
-	{
-		powerUpXpos--;
-	}
-	*/
 }
 
 void gmplResolveObjectCollision()
@@ -131,44 +136,14 @@ void gmplResolveObjectCollision()
 	}
 	
 	
-	/*
-	if(playerYPos == 0 && obstacles[0][0]!=0)
-	{
-		HpDown();	
-	}
-	if(playerYPos == 1&& obstacles[1][0]!=0)
-	{
-		HpDown();
-	}
 	
-	if(playerYPos == 0 && bullets[0][0]!=0)
-	{
-		HpDown();	
-	}
-	if(playerYPos == 1&& bullets[1][0]!=0)
-	{
-		HpDown();
-	}
-	
-	if(playerYPos == powerUpYPos && powerUpXPos == 0)
-	{
-		//do napisania: funkcje do odpalania PowerUpów
-		switch(powUpType)
-		{
-			case HPUp:
-				
-				break;
-			case Defense:
-				
-				break;
-			case LaserPowerUp:
-				
-				break;
-			case DecreaseSpeed:
-				
-				break;
-		}
-	}*/
 	
 }
-
+void HpDown()
+{
+	lifesLeft--;
+	if(lifesLeft==0)
+	{
+		gameOver();	
+	}
+}
